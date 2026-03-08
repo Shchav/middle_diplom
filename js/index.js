2ae3e9ea7d5f4f164a3d57dd06ee0dcad8234d5d
Andrew Shchelykalnov <shchav@mail.ru>
2026-03-08 13:32:15 +0300

    End

--- js/index.js
+++ js/index.js
@@ -4,10 +4,8 @@ import leaveRequest from "./modules/leaveRequest.js";
 import { calc } from "./modules/calc.js";
 import { scrolling } from "./modules/scrolling.js";
 import { sertificates } from "./modules/sertificates.js";
+import { sliderCarousel } from "./modules/sliderCarousel.js";
 
-// Задача 6.
-// Время, когда закончатся скидки
-timer('8 march 2026 12:40:00')
 // Задача 2.
 // Работа с модальным окном "Заказать звонок"
 modals({
@@ -15,6 +13,46 @@ modals({
     modalSlct: '.header-modal',
     overlayModalSlct: '.overlay',
     closeModalBtnSlct: '.header-modal__close'
+})
+// Задача 3.
+// Слайдер-карусель "Выгоды для клиентов"
+sliderCarousel({
+    items: '.benefits__item',
+    itemsWrap: '.benefits-wrap',
+    arrowsWrap: '.benefits-arrows',
+    arrowCls: '.benefits__arrow',
+    arrowLeft: 'benefits__arrow--left',
+    arrowRight: 'benefits__arrow--right',
+    numItems: [
+        {
+            num: 3,
+            betweenWidth: [1576, Infinity]
+        },
+        {
+            num: 2,
+            betweenWidth: [0, 1576]
+        }
+    ]
+})
+// Задача 4.
+// Слайдер-карусель "Наши услуги"
+sliderCarousel({
+    items: '.col-md-12',
+    itemsWrap: '.service-block-wrap',
+    arrowsWrap: '.services-arrows',
+    arrowCls: '.services__arrow',
+    arrowLeft: 'services__arrow--left',
+    arrowRight: 'services__arrow--right',
+    numItems: [
+        {
+            num: 2,
+            betweenWidth: [1576, Infinity]
+        },
+        {
+            num: 1,
+            betweenWidth: [0, 1576]
+        }
+    ]
 })
 // Задача 5.
 // Работа с модальным окном "Вызвать замерщика"
@@ -24,18 +62,24 @@ modals({
     overlayModalSlct: '.overlay',
     closeModalBtnSlct: '.services-modal__close'
 })
+// Задача 6.
+// Время, когда закончатся скидки
+timer('8 march 2026 12:40:00')
 // Задача 7.
 // Надо установить валидный url для отправки заявки
 leaveRequest('https://jsonplaceholder.typicode.com/posts')
-// Задача 10.
-calc()
+// Задача 8.
+// Сертификаты
+sertificates()
 // Задача 9.
 // Плавная прокрутка страницы
 scrolling({
     smoothScrollCls: '.smooth-scroll', // Кнопка прокрутки страницы на самый верх
     timeScrol: 1000 // Время плавной прокрутки
 })
-// Задача 8.
-// Сертификаты
-sertificates()
+// Задача 10.
+// Калькулятор
+calc()
+
+
 
